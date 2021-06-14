d5a2728e1b81fc8eb7a51f65b1d73fe9be2f2780
Carlos <karloscal2013@gmail.com>
2021-06-14 13:22:09 -0300

    task: Se sube al repositorio la estructura del proyecto final

---
+++ Prog_Visual_2021_TP_Final/src/main/java/ar/edu/unju/fi/tpfinal/ProgVisual2021TpFinalApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.tpfinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgVisual2021TpFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgVisual2021TpFinalApplication.class, args);
+	}
+
+}
